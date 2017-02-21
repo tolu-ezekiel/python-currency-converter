60b02c0f706e3ef4a7d0134c1ce56d5a986b23d3
tolu-ezekiel <tolulope.awoyemi@andela.co>
2017-02-21 22:20:47 +0100

    created convert currency in separate definition

--- currency_converter.py
+++ currency_converter.py
@@ -12,11 +12,11 @@ Options:
 
 from docopt import docopt
 import json
+import requests
 
 with open('currency_code.json') as data_file:    
     currency_code = json.load(data_file)
     
-
 def check_currency_code(currency_code, currency, total_code):
     for i in range(total_code):
         if currency == currency_code['code'][i]['symbol']:
@@ -24,10 +24,17 @@ def check_currency_code(currency_code, currency, total_code):
             break
     return currency
 
+def convert_currency(input_currency, output_currency, amount):
+    token = "0EFC588CBFB84BE9B5E63D7372B6B71922"
+    r = requests.get('http://globalcurrencies.xignite.com/xGlobalCurrencies.json/ConvertRealTimeValue?_token='+ token +'&From='+ input_currency +'&To=' + output_currency +'&Amount=' + amount)
+    if json.loads(r.content)['Outcome'] == 'Success':
+        conversion_result = json.loads(r.content)['Result']
+        return conversion_result
+    else:
+        return json.loads(r.content)['Outcome']
 
 def main():
     arguments = docopt(__doc__)
-
     amount = arguments.get('--amount')
     input_currency = arguments.get('--input_currency')
     output_currency = arguments.get('--output_currency')
@@ -35,11 +42,38 @@ def main():
     total_code =  len(currency_code['code'])
     if input_currency.isalpha() == False or len(input_currency) < 3:
         input_currency = check_currency_code(currency_code, input_currency , total_code)
-    if output_currency.isalpha() == False or len(output_currency) < 3:
-        output_currency = check_currency_code(currency_code, input_currency , total_code)
+    result = {
+        "input": {
+            "amount": amount,
+            "currency": input_currency
+        },
+        "output": {
+        }
+    }
+    if output_currency:
+        if output_currency.isalpha() == False or len(output_currency) < 3:
+            output_currency = check_currency_code(currency_code, input_currency , total_code)
+        conversion_result = convert_currency(input_currency, output_currency, amount)
+        result['output'].update({'output_currency': conversion_result})
+        print result
+    # else:
+    #     token = "0EFC588CBFB84BE9B5E63D7372B6B71922"
+    #     for i in range(total_code):
+    #         output_currency = currency_code['code'][i]['letter']
+    #         if output_currency != input_currency:
 
-    print 'input_currency', input_currency
-    print 'output_currency', output_currency
+    #     r = requests.get('http://globalcurrencies.xignite.com/xGlobalCurrencies.json/ConvertRealTimeValue?_token='+ token +'&From='+ input_currency +'&To=' + output_currency +'&Amount=' + amount)
+    #     if json.loads(r.content)['Outcome'] == 'Success':
+    #         result = {
+    #             "input": {
+    #                 "amount": amount,
+    #                 "currency": input_currency
+    #             },
+    #             "output": {
+    #                 output_currency: json.loads(r.content)['Result'], 
+    #             }
+    #         }
+    #         print result
 
 
 if __name__ == '__main__':
