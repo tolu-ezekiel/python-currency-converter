613b25344c2a763ab44c71299943154938f01f92
tolu-ezekiel <tolulope.awoyemi@andela.co>
2017-02-21 17:57:49 +0100

    added docopt

---
+++ currency_converter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+Usage:
+    new2.py [--amount=<amount>] [--input_currency=<input_currency>] [--output_currency=<output_currency>] 
+
+Options:
+    --amount=<amount>                       Amount
+    --input_currency=<input_currency>       inputCurrency
+    --output_currency=<output_currency>     outputCurrency
+"""
+
+from docopt import docopt
+
+
+def main():
+    arguments = docopt(__doc__)
+
+    amount = arguments.get('--amount')
+    input_currency = arguments.get('--input_currency')
+    output_currency = arguments.get('--output_currency')
+
+    print 'amount', amount
+    print 'input_currency', input_currency
+    print 'output_currency', output_currency
+
+if __name__ == '__main__':
+    main()
