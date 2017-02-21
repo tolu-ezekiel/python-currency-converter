f2e016a32d240ffbc9e5c028005206f305bace27
tolu-ezekiel <tolulope.awoyemi@andela.co>
2017-02-21 19:14:17 +0100

    fetch currency code using surrency symbol

--- currency_converter.py
+++ currency_converter.py
@@ -17,6 +17,14 @@ with open('currency_code.json') as data_file:
     currency_code = json.load(data_file)
     
 
+def check_currency_code(currency_code, currency, total_code):
+    for i in range(total_code):
+        if currency == currency_code['code'][i]['symbol']:
+            currency = currency_code['code'][i]['letter']
+            break
+    return currency
+
+
 def main():
     arguments = docopt(__doc__)
 
@@ -24,9 +32,15 @@ def main():
     input_currency = arguments.get('--input_currency')
     output_currency = arguments.get('--output_currency')
 
-    print 'amount', amount
+    total_code =  len(currency_code['code'])
+    if input_currency.isalpha() == False or len(input_currency) < 3:
+        input_currency = check_currency_code(currency_code, input_currency , total_code)
+    if output_currency.isalpha() == False or len(output_currency) < 3:
+        output_currency = check_currency_code(currency_code, input_currency , total_code)
+
     print 'input_currency', input_currency
     print 'output_currency', output_currency
+
 
 if __name__ == '__main__':
     main()
